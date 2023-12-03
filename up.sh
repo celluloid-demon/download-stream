fdaed8302f7145ae7c4bfd0b686ebe2bbcf9d498
jonathan <celluloid-demon@users.noreply.github.com>
2023-12-03 15:26:02 -0800

    bug fixes

--- up.sh
+++ up.sh
@@ -28,7 +28,7 @@ init() {
 	OS_TYPE="$(get_os_type)"
 
 	[ "$OS_TYPE" == "LINUX" ]		&& DOCKER_CMD="docker"
-	[ "$OS_TYPE" == "MACOS" ]		&& DOCKER_CMD="/Applications/Docker.app/Contents/Resources/docker"
+	[ "$OS_TYPE" == "MACOS" ]		&& echo "macOS not supported (because I don't have a mac to test on), terminating." && exit 1
 	[ "$OS_TYPE" == "WSL" ]			&& DOCKER_CMD="/mnt/c/Program\ Files/Docker/Docker/Docker\ Desktop.exe"
 	[ "$OS_TYPE" == "WINDOWS" ]		&& echo "Unsupported runtime (please run from WSL instead), terminating." && exit 1
 	[ "$OS_TYPE" == "UNKNOWN_OS" ]	&& echo "Unknown OS type ('uname -s' output: $(uname -s)), terminating." && exit 1
@@ -57,8 +57,9 @@ main() {
 
 	init
 	
-	# Start docker engine if there is no PID under the process name of 'docker'
-	if ! pidof docker > /dev/null; then
+	# WSL: Start Docker Desktop for Windows engine if there is no PID under the
+	# process name of 'docker'
+	if [ "$OS_TYPE" == "WSL" ] && ! pidof docker > /dev/null; then
 
 		echo "Starting docker..."
 
